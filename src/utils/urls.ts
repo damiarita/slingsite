7a6912e1d3b8e63f3fb227cba6ae04f243a11b90
Damià Rita <damiarita@gmail.com>
2025-09-04 00:30:24 +0200

    Move getUrlsByLocale to a utils file

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,4 +1,4 @@
-import { Locale } from "@/i18n/lib"
+import { Locale, locales } from "@/i18n/lib"
 
 export const pageTypes = ['image'] as const;
 export type PageType = typeof pageTypes[number]
@@ -12,3 +12,11 @@ export const getUrl = (locale:Locale, pageType:PageType, queryParams?:string):st
     return `/${locale}/${path}/${queryParams||''}`
 }
     
+export const getUrlsByLocale = (pageType:PageType): Record<Locale, string>=>
+    locales.reduce(
+    function(acc, locale){
+        acc[locale]=getUrl(locale, pageType)
+        return acc
+    },
+    {} as Record<Locale, string>
+)
