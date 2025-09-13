e686ff53b0411f86d927617772045297aa131a5a
Damià Rita Espada <damiarita@gmail.com>
2025-09-14 01:04:10 +0200

    Add suscribe page

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,12 +1,13 @@
-import { getTranslations } from "@/content/lib";
+import { getPostOfId, getTranslations } from "@/content/lib";
 import { Locale, locales } from "@/i18n/lib"
 import { Post } from "contentlayer/generated";
 
-export const pageTypes = ['image'] as const;
+export const pageTypes = ['image', 'suscribe'] as const;
 export type PageType = typeof pageTypes[number]
 
 const pagePaths:Record<PageType, string> = {
-    image: "image"
+    image: "image",
+    suscribe: "suscribe"
 }
 
 export const getUrl = (locale:Locale, pageType:PageType, queryParams?:string):string=>{
@@ -23,8 +24,12 @@ export const getUrlsByLocale = (pageType:PageType): Record<Locale, string>=>
     {} as Record<Locale, string>
 )
 
-export const getPostUrl = (post:Post, queryParams?:string):string=>{
-    return `/${post.locale}/${post.slug}/${queryParams||''}`
+export function getPostUrl(post: Post): string;
+export function getPostUrl(id:string, locale: Locale): string;
+export function getPostUrl(postOrId: Post | string, locale?: Locale): string {
+    const post = typeof postOrId === 'string'&& locale ? getPostOfId(postOrId, locale): postOrId as Post;
+    if (!post) throw new Error('Post not found');
+    return `/${post.locale}/${post.slug}/`
 }
 
 export const getPostUrlsByLocale = (post:Post): Record<Locale, string>=>{
