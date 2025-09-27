f5040b3f31405c9a2e958b32442ed2c312f54b1c
Damià Rita Espada <damiarita@gmail.com>
2025-09-27 23:19:55 +0200

    Install packages for video compression

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,28 @@
 import type { NextConfig } from "next";
 import { withContentlayer } from "next-contentlayer2";
+import CopyPlugin from "copy-webpack-plugin";
 
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
+  },
+  productionBrowserSourceMaps: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.plugins.push(
+        new CopyPlugin({
+          patterns: [
+            {
+              from: 'node_modules/@ffmpeg/core/dist/umd',
+              to: 'static/ffmpeg'
+            }
+          ]
+        })
+      );
+    }
+    return config;
   }
 };
 
