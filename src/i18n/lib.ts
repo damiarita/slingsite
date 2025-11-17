e775d78fe685f67ec539b5a50738030ce2668099
Damià Rita Espada <damiarita@gmail.com>
2025-11-17 22:51:06 +0100

    DRY i18n lib

--- src/i18n/lib.ts
+++ src/i18n/lib.ts
@@ -17,238 +17,102 @@ export type Locale = (typeof locales)[number];
 export const localeIsValid = (locale: string) =>
   locales.includes(locale as Locale);
 
-export const getCookieDictionary = async (
-  locale: Locale,
-): Promise<CookieConsentTranslations> => {
-  if (locale === 'ar') {
-    return (await import('./dictioraries/cookies/ar.json')).default;
-  }
-  if (locale === 'de') {
-    return (await import('./dictioraries/cookies/de.json')).default;
-  }
-  if (locale === 'es') {
-    return (await import('./dictioraries/cookies/es.json')).default;
-  }
-  if (locale === 'fr') {
-    return (await import('./dictioraries/cookies/fr.json')).default;
-  }
-  if (locale === 'it') {
-    return (await import('./dictioraries/cookies/it.json')).default;
-  }
-  if (locale === 'zh') {
-    return (await import('./dictioraries/cookies/zh.json')).default;
-  }
-  return (await import('./dictioraries/cookies/en.json')).default;
-};
-export const getBlogDictionary = async (
-  locale: Locale,
-): Promise<BlogTranslations> => {
-  if (locale === 'ar') {
-    return (await import('./dictioraries/blog/ar.json')).default;
-  }
-  if (locale === 'de') {
-    return (await import('./dictioraries/blog/de.json')).default;
-  }
-  if (locale === 'es') {
-    return (await import('./dictioraries/blog/es.json')).default;
-  }
-  if (locale === 'fr') {
-    return (await import('./dictioraries/blog/fr.json')).default;
-  }
-  if (locale === 'it') {
-    return (await import('./dictioraries/blog/it.json')).default;
-  }
-  if (locale === 'zh') {
-    return (await import('./dictioraries/blog/zh.json')).default;
-  }
-  return (await import('./dictioraries/blog/en.json')).default;
-};
-export const getVideoPageMetadataDictionary = async (
-  locale: Locale,
-): Promise<PageMetadata> => {
-  if (locale === 'ar') {
-    return (await import('./dictioraries/compression/video/metadata/ar.json'))
-      .default;
-  }
-  if (locale === 'de') {
-    return (await import('./dictioraries/compression/video/metadata/de.json'))
-      .default;
-  }
-  if (locale === 'es') {
-    return (await import('./dictioraries/compression/video/metadata/es.json'))
-      .default;
-  }
-  if (locale === 'fr') {
-    return (await import('./dictioraries/compression/video/metadata/fr.json'))
-      .default;
-  }
-  if (locale === 'it') {
-    return (await import('./dictioraries/compression/video/metadata/it.json'))
-      .default;
-  }
-  if (locale === 'zh') {
-    return (await import('./dictioraries/compression/video/metadata/zh.json'))
-      .default;
-  }
-  return (await import('./dictioraries/compression/video/metadata/en.json'))
-    .default;
-};
-export const getImagePageMetadataDictionary = async (
-  locale: Locale,
-): Promise<PageMetadata> => {
-  if (locale === 'ar') {
-    return (await import('./dictioraries/compression/image/metadata/ar.json'))
-      .default;
-  }
-  if (locale === 'de') {
-    return (await import('./dictioraries/compression/image/metadata/de.json'))
-      .default;
-  }
-  if (locale === 'es') {
-    return (await import('./dictioraries/compression/image/metadata/es.json'))
-      .default;
-  }
-  if (locale === 'fr') {
-    return (await import('./dictioraries/compression/image/metadata/fr.json'))
-      .default;
-  }
-  if (locale === 'it') {
-    return (await import('./dictioraries/compression/image/metadata/it.json'))
-      .default;
-  }
-  if (locale === 'zh') {
-    return (await import('./dictioraries/compression/image/metadata/zh.json'))
-      .default;
-  }
-  return (await import('./dictioraries/compression/image/metadata/en.json'))
-    .default;
-};
-export const getImagePageSeoDictionary = async (
-  locale: Locale,
-): Promise<CompressionPageSeoTranslations> => {
-  if (locale === 'ar') {
-    return (await import('./dictioraries/compression/image/seo/ar.json'))
-      .default;
-  }
-  if (locale === 'de') {
-    return (await import('./dictioraries/compression/image/seo/de.json'))
-      .default;
-  }
-  if (locale === 'es') {
-    return (await import('./dictioraries/compression/image/seo/es.json'))
-      .default;
-  }
-  if (locale === 'fr') {
-    return (await import('./dictioraries/compression/image/seo/fr.json'))
-      .default;
-  }
-  if (locale === 'it') {
-    return (await import('./dictioraries/compression/image/seo/it.json'))
-      .default;
-  }
-  if (locale === 'zh') {
-    return (await import('./dictioraries/compression/image/seo/zh.json'))
-      .default;
-  }
-  return (await import('./dictioraries/compression/image/seo/en.json')).default;
-};
-export const getVideoPageSeoDictionary = async (
-  locale: Locale,
-): Promise<CompressionPageSeoTranslations> => {
-  if (locale === 'ar') {
-    return (await import('./dictioraries/compression/video/seo/ar.json'))
-      .default;
-  }
-  if (locale === 'de') {
-    return (await import('./dictioraries/compression/video/seo/de.json'))
-      .default;
-  }
-  if (locale === 'es') {
-    return (await import('./dictioraries/compression/video/seo/es.json'))
-      .default;
-  }
-  if (locale === 'fr') {
-    return (await import('./dictioraries/compression/video/seo/fr.json'))
-      .default;
-  }
-  if (locale === 'it') {
-    return (await import('./dictioraries/compression/video/seo/it.json'))
-      .default;
-  }
-  if (locale === 'zh') {
-    return (await import('./dictioraries/compression/video/seo/zh.json'))
-      .default;
-  }
-  return (await import('./dictioraries/compression/video/seo/en.json')).default;
-};
-export const getRedirectionDictionary = async (
-  locale: Locale,
-): Promise<RedirectingTranslations> => {
-  if (locale === 'ar') {
-    return (await import('./dictioraries/redirecter/ar.json')).default;
-  }
-  if (locale === 'de') {
-    return (await import('./dictioraries/redirecter/de.json')).default;
-  }
-  if (locale === 'es') {
-    return (await import('./dictioraries/redirecter/es.json')).default;
-  }
-  if (locale === 'fr') {
-    return (await import('./dictioraries/redirecter/fr.json')).default;
-  }
-  if (locale === 'it') {
-    return (await import('./dictioraries/redirecter/it.json')).default;
-  }
-  if (locale === 'zh') {
-    return (await import('./dictioraries/redirecter/zh.json')).default;
-  }
-  return (await import('./dictioraries/redirecter/en.json')).default;
-};
+type DictionaryImporter<T> = Record<Locale, () => Promise<{ default: T }>>;
+function createDictionaryGetter<T>(importers: DictionaryImporter<T>) {
+  return async (locale: Locale): Promise<T> => {
+    const importer = importers[locale] || importers['en'];
+    return (await importer()).default;
+  };
+}
 
-export const getNavBarDictionary = async (
-  locale: Locale,
-): Promise<NavBarTranslations> => {
-  if (locale === 'ar') {
-    return (await import('./dictioraries/nav-bar/ar.json')).default;
-  }
-  if (locale === 'de') {
-    return (await import('./dictioraries/nav-bar/de.json')).default;
-  }
-  if (locale === 'es') {
-    return (await import('./dictioraries/nav-bar/es.json')).default;
-  }
-  if (locale === 'fr') {
-    return (await import('./dictioraries/nav-bar/fr.json')).default;
-  }
-  if (locale === 'it') {
-    return (await import('./dictioraries/nav-bar/it.json')).default;
-  }
-  if (locale === 'zh') {
-    return (await import('./dictioraries/nav-bar/zh.json')).default;
-  }
-  return (await import('./dictioraries/nav-bar/en.json')).default;
-};
+export const getCookieDictionary =
+  createDictionaryGetter<CookieConsentTranslations>({
+    ar: () => import('./dictioraries/cookies/ar.json'),
+    de: () => import('./dictioraries/cookies/de.json'),
+    es: () => import('./dictioraries/cookies/es.json'),
+    fr: () => import('./dictioraries/cookies/fr.json'),
+    it: () => import('./dictioraries/cookies/it.json'),
+    zh: () => import('./dictioraries/cookies/zh.json'),
+    en: () => import('./dictioraries/cookies/en.json'),
+  });
 
-export const getFooterDictionary = async (
-  locale: Locale,
-): Promise<FooterTranslations> => {
-  if (locale === 'ar') {
-    return (await import('./dictioraries/footer/ar.json')).default;
-  }
-  if (locale === 'de') {
-    return (await import('./dictioraries/footer/de.json')).default;
-  }
-  if (locale === 'es') {
-    return (await import('./dictioraries/footer/es.json')).default;
-  }
-  if (locale === 'fr') {
-    return (await import('./dictioraries/footer/fr.json')).default;
-  }
-  if (locale === 'it') {
-    return (await import('./dictioraries/footer/it.json')).default;
-  }
-  if (locale === 'zh') {
-    return (await import('./dictioraries/footer/zh.json')).default;
-  }
-  return (await import('./dictioraries/footer/en.json')).default;
-};
+export const getBlogDictionary = createDictionaryGetter<BlogTranslations>({
+  ar: () => import('./dictioraries/blog/ar.json'),
+  de: () => import('./dictioraries/blog/de.json'),
+  es: () => import('./dictioraries/blog/es.json'),
+  fr: () => import('./dictioraries/blog/fr.json'),
+  it: () => import('./dictioraries/blog/it.json'),
+  zh: () => import('./dictioraries/blog/zh.json'),
+  en: () => import('./dictioraries/blog/en.json'),
+});
+
+export const getVideoPageMetadataDictionary =
+  createDictionaryGetter<PageMetadata>({
+    ar: () => import('./dictioraries/compression/video/metadata/ar.json'),
+    de: () => import('./dictioraries/compression/video/metadata/de.json'),
+    es: () => import('./dictioraries/compression/video/metadata/es.json'),
+    fr: () => import('./dictioraries/compression/video/metadata/fr.json'),
+    it: () => import('./dictioraries/compression/video/metadata/it.json'),
+    zh: () => import('./dictioraries/compression/video/metadata/zh.json'),
+    en: () => import('./dictioraries/compression/video/metadata/en.json'),
+  });
+export const getImagePageMetadataDictionary =
+  createDictionaryGetter<PageMetadata>({
+    ar: () => import('./dictioraries/compression/image/metadata/ar.json'),
+    de: () => import('./dictioraries/compression/image/metadata/de.json'),
+    es: () => import('./dictioraries/compression/image/metadata/es.json'),
+    fr: () => import('./dictioraries/compression/image/metadata/fr.json'),
+    it: () => import('./dictioraries/compression/image/metadata/it.json'),
+    zh: () => import('./dictioraries/compression/image/metadata/zh.json'),
+    en: () => import('./dictioraries/compression/image/metadata/en.json'),
+  });
+export const getImagePageSeoDictionary =
+  createDictionaryGetter<CompressionPageSeoTranslations>({
+    ar: () => import('./dictioraries/compression/image/seo/ar.json'),
+    de: () => import('./dictioraries/compression/image/seo/de.json'),
+    es: () => import('./dictioraries/compression/image/seo/es.json'),
+    fr: () => import('./dictioraries/compression/image/seo/fr.json'),
+    it: () => import('./dictioraries/compression/image/seo/it.json'),
+    zh: () => import('./dictioraries/compression/image/seo/zh.json'),
+    en: () => import('./dictioraries/compression/image/seo/en.json'),
+  });
+export const getVideoPageSeoDictionary =
+  createDictionaryGetter<CompressionPageSeoTranslations>({
+    ar: () => import('./dictioraries/compression/video/seo/ar.json'),
+    de: () => import('./dictioraries/compression/video/seo/de.json'),
+    es: () => import('./dictioraries/compression/video/seo/es.json'),
+    fr: () => import('./dictioraries/compression/video/seo/fr.json'),
+    it: () => import('./dictioraries/compression/video/seo/it.json'),
+    zh: () => import('./dictioraries/compression/video/seo/zh.json'),
+    en: () => import('./dictioraries/compression/video/seo/en.json'),
+  });
+export const getRedirectionDictionary =
+  createDictionaryGetter<RedirectingTranslations>({
+    ar: () => import('./dictioraries/redirecter/ar.json'),
+    de: () => import('./dictioraries/redirecter/de.json'),
+    es: () => import('./dictioraries/redirecter/es.json'),
+    fr: () => import('./dictioraries/redirecter/fr.json'),
+    it: () => import('./dictioraries/redirecter/it.json'),
+    zh: () => import('./dictioraries/redirecter/zh.json'),
+    en: () => import('./dictioraries/redirecter/en.json'),
+  });
+
+export const getNavBarDictionary = createDictionaryGetter<NavBarTranslations>({
+  ar: () => import('./dictioraries/nav-bar/ar.json'),
+  de: () => import('./dictioraries/nav-bar/de.json'),
+  es: () => import('./dictioraries/nav-bar/es.json'),
+  fr: () => import('./dictioraries/nav-bar/fr.json'),
+  it: () => import('./dictioraries/nav-bar/it.json'),
+  zh: () => import('./dictioraries/nav-bar/zh.json'),
+  en: () => import('./dictioraries/nav-bar/en.json'),
+});
+
+export const getFooterDictionary = createDictionaryGetter<FooterTranslations>({
+  ar: () => import('./dictioraries/footer/ar.json'),
+  de: () => import('./dictioraries/footer/de.json'),
+  es: () => import('./dictioraries/footer/es.json'),
+  fr: () => import('./dictioraries/footer/fr.json'),
+  it: () => import('./dictioraries/footer/it.json'),
+  zh: () => import('./dictioraries/footer/zh.json'),
+  en: () => import('./dictioraries/footer/en.json'),
+});
