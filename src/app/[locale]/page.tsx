edc94d6e4842a18272952ff1a1e104839705a70d
Damià Rita Espada <damiarita@gmail.com>
2025-12-21 23:58:12 +0100

    Improve social metadata in redirecting pages

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,14 +1,49 @@
-import { getRedirectionDictionary, Locale } from '@/i18n/lib';
+import {
+  getImagePageMetadataDictionary,
+  getRedirectionDictionary,
+  Locale,
+} from '@/i18n/lib';
 import { Redirecter } from '@/components/redirecter';
 import { Metadata } from 'next';
+import { getUrl } from '@/utils/urls';
 
 type Props = { params: Promise<{ locale: Locale }> };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { locale } = await params;
-  const dict = await getRedirectionDictionary(locale);
+  const redirectionDictionary = await getRedirectionDictionary(locale);
+  const destinationPageTranslation =
+    await getImagePageMetadataDictionary(locale);
   return {
-    title: dict.redirecting,
+    title: redirectionDictionary.redirecting,
+    openGraph: {
+      title: destinationPageTranslation.title,
+      description: destinationPageTranslation.description,
+      url: getUrl(locale, 'image'),
+      siteName: 'SlingSite',
+      images: [
+        {
+          url: '/screenshots/image.jpg',
+          width: 910,
+          height: 465,
+          alt: 'SlingSite Logo',
+        },
+        {
+          url: '/favicon.ico',
+          width: 256,
+          height: 256,
+          alt: 'SlingSite Logo',
+        },
+      ],
+      locale,
+      type: 'website',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: destinationPageTranslation.title,
+      description: destinationPageTranslation.description,
+      images: ['/screenshots/image.jpg', '/favicon.ico'],
+    },
   };
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,44 @@
 import { Redirecter } from '@/components/redirecter';
-import { getRedirectionDictionary } from '@/i18n/lib';
+import {
+  getImagePageMetadataDictionary,
+  getRedirectionDictionary,
+} from '@/i18n/lib';
+import { getUrl } from '@/utils/urls';
 import { Metadata } from 'next';
 
 export async function generateMetadata(): Promise<Metadata> {
-  const dict = await getRedirectionDictionary('en');
+  const redirectionDictionary = await getRedirectionDictionary('en');
+  const destinationPageTranslation = await getImagePageMetadataDictionary('en');
   return {
-    title: dict.redirecting,
+    title: redirectionDictionary.redirecting,
+    openGraph: {
+      title: destinationPageTranslation.title,
+      description: destinationPageTranslation.description,
+      url: getUrl('en', 'image'),
+      siteName: 'SlingSite',
+      images: [
+        {
+          url: '/screenshots/image.jpg',
+          width: 910,
+          height: 465,
+          alt: 'SlingSite Logo',
+        },
+        {
+          url: '/favicon.ico',
+          width: 256,
+          height: 256,
+          alt: 'SlingSite Logo',
+        },
+      ],
+      locale: 'en',
+      type: 'website',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: destinationPageTranslation.title,
+      description: destinationPageTranslation.description,
+      images: ['/screenshots/image.jpg', '/favicon.ico'],
+    },
   };
 }
 
