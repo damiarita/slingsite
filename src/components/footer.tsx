4f931c8c584b05309bec3f91653bfe6c846256ad
Damià Rita Espada <damiarita@gmail.com>
2026-08-11 00:10:26 +0200

    Add new link to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -62,6 +62,13 @@ export default function Footer({
                     locale={locale}
                   />
                 </li>
+                <li>
+                  <BlogLink
+                    postId="tiny-png.mdx"
+                    className="text-base text-gray-500 hover:text-gray-900"
+                    locale={locale}
+                  />
+                </li>
               </ul>
             </div>
             <div className="mt-12 md:mt-0">
