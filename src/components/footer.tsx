bc89ad6995a12874685a4779626be5f0ab6b14ea
Damià Rita <damiarita@gmail.com>
2026-09-06 00:01:47 +0000

    Add cloudinary comparison article

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -24,6 +24,71 @@ export default function Footer({
         </div>
         <div className="mt-12 grid grid-cols-2 gap-8 xl:mt-0 xl:col-span-2">
           <div className="md:grid md:grid-cols-2 md:gap-8">
+            <div className="mt-12 md:mt-0">
+              <h2 className="text-sm font-semibold text-gray-500 tracking-wider uppercase">
+                {translations.headings.legal}
+              </h2>
+              <ul className="mt-4 space-y-4">
+                <li>
+                  <BlogLink
+                    postId="privacy.mdx"
+                    className="text-base text-gray-500 hover:text-gray-900"
+                    locale={locale}
+                  />
+                </li>
+                <li>
+                  <BlogLink
+                    postId="short-t-and-c.mdx"
+                    className="text-base text-gray-500 hover:text-gray-900"
+                    locale={locale}
+                  />
+                </li>
+                <li>
+                  <BlogLink
+                    postId="t-and-c.mdx"
+                    className="text-base text-gray-500 hover:text-gray-900"
+                    locale={locale}
+                  />
+                </li>
+                <li>
+                  <button
+                    data-cc="show-preferencesModal"
+                    className="text-base text-gray-500 hover:text-gray-900"
+                  >
+                    {translations.cookies}
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="md:grid md:grid-cols-2 md:gap-8">
+            <div>
+              <h2 className="text-sm font-semibold text-gray-500 tracking-wider uppercase">
+                {translations.headings.development}
+              </h2>
+              <ul className="mt-4 space-y-4">
+                <li>
+                  <a
+                    href="https://github.com/damiarita/slingsite"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-flex items-center text-base text-gray-500 hover:text-gray-900"
+                  >
+                    {translations.sourceCode}
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://github.com/damiarita/slingsite/issues"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-flex items-center text-base text-gray-500 hover:text-gray-900"
+                  >
+                    {translations.issues}
+                  </a>
+                </li>
+              </ul>
+            </div>
             <div>
               <h2 className="text-sm font-semibold text-gray-500 tracking-wider uppercase">
                 {translations.headings.connect}
@@ -90,70 +155,12 @@ export default function Footer({
                     locale={locale}
                   />
                 </li>
-              </ul>
-            </div>
-            <div className="mt-12 md:mt-0">
-              <h2 className="text-sm font-semibold text-gray-500 tracking-wider uppercase">
-                {translations.headings.legal}
-              </h2>
-              <ul className="mt-4 space-y-4">
                 <li>
                   <BlogLink
-                    postId="privacy.mdx"
+                    postId="cloudinary.mdx"
                     className="text-base text-gray-500 hover:text-gray-900"
                     locale={locale}
                   />
-                </li>
-                <li>
-                  <BlogLink
-                    postId="short-t-and-c.mdx"
-                    className="text-base text-gray-500 hover:text-gray-900"
-                    locale={locale}
-                  />
-                </li>
-                <li>
-                  <BlogLink
-                    postId="t-and-c.mdx"
-                    className="text-base text-gray-500 hover:text-gray-900"
-                    locale={locale}
-                  />
-                </li>
-                <li>
-                  <button
-                    data-cc="show-preferencesModal"
-                    className="text-base text-gray-500 hover:text-gray-900"
-                  >
-                    {translations.cookies}
-                  </button>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="md:grid md:grid-cols-2 md:gap-8">
-            <div>
-              <h2 className="text-sm font-semibold text-gray-500 tracking-wider uppercase">
-                {translations.headings.development}
-              </h2>
-              <ul className="mt-4 space-y-4">
-                <li>
-                  <a
-                    href="https://github.com/damiarita/slingsite"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="inline-flex items-center text-base text-gray-500 hover:text-gray-900"
-                  >
-                    {translations.sourceCode}
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://github.com/damiarita/slingsite/issues"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="inline-flex items-center text-base text-gray-500 hover:text-gray-900"
-                  >
-                    {translations.issues}
-                  </a>
                 </li>
               </ul>
             </div>
